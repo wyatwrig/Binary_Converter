b446bcb4f1fb695ec70f48093a03f6f16841dd91
wyatwrig <wyatwrig@gmail.com>
2020-12-01 20:39:10 -0700

    added repl and started binaryToDecimal

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -1,4 +1,41 @@
+import math
+
+def conversionType():
+    choice = '0'
+    while (choice != '3'):
+        print("Choose an option:")
+        print("1. Binary to Decimal")
+        print("2. Decimal to Binary")
+        print("3. Quit")
+        choice = input(">>")
+        if (choice == '1'):
+            binaryToDecimal()
+        elif (choice == '2'):
+            decimalToBinary()
+        elif (choice == '3'):
+            print("Exiting...")
+            return
+        else:
+            print("Not a Valid Choice")
+
+def getNumber():
+    return int(input("Enter a number: "))
+
+def binaryToDecimal():
+    number = getNumber()
+    binary = 0
+    totalDigits = int(math.log10(number)) + 1
+    digitsArray = [int(i) for i in str(number)]
+
+    for digit in digitsArray:
+        if (digit == 1):
+            binary += (pow(2, totalDigits) - 1)
+        totalDigits -= 1
+
+    print(str(number) + " is " + str(binary) + " in decimal.")
 
 
-def BinaryConverter():
-    return
+def decimalToBinary():
+    number = getNumber()
+
+conversionType()
